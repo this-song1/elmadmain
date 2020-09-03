80bd64ff55124bc8ac1ec1e55c998b563253d3cb
this-song1 <68591103+this-song1@users.noreply.github.com>
2020-09-03 10:39:24 +0800

    111

---
+++ src/s1.java
@@ -0,0 +1,15 @@
+
+
+    public  class s1 {
+
+        private final static s1 INSTANCE = new s1();
+
+        private s1(){}
+
+        public static s1 getInstance(){
+            return INSTANCE;
+        }
+    }
+
+
+
